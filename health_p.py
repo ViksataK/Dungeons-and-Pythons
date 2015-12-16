01cb53281d0134561f1e2efc55b9e6548cfa02aa
ViksataK <viktor.klisurov@gmail.com>
2015-12-16 19:29:59 +0200

    adding mana and health potions

---
+++ health_p.py
@@ -0,0 +1,6 @@
+class Health_p:
+    def __init__(self):
+        self._healthGiven = 20
+
+    def heal_by_potion(self):
+        return self._healthGiven
---
+++ mana_p.py
@@ -0,0 +1,6 @@
+class Mana_p:
+    def __init__(self):
+        self._manaGiven = 20
+
+    def get_mana(self):
+        return self._manaGiven
